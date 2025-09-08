77222d4f2c2c7c9aea911eb168b9d6bba9e21ac1
˹ 𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 ˼ <skr346902@gmail.com>
2025-09-08 17:54:25 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "neon-blue": "#0ff",
+        "neon-pink": "#ff00cc",
+        "neon-green": "#39ff14",
+        "neon-orange": "#ff9900"
+      }
+    }
+  },
+  plugins: []
+}
